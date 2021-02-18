0dfec7f31f8d8247868a2f37316ad7145c5ab9ab
zanahp <zparet@yahoo.com>
2021-02-18 18:27:03 -0500

    fixed weather icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,31 +1,46 @@
 import React from "react";
-import { WeatherIcon } from "weather-icons-react";
+import {
+  WiDaySunny,
+  WiNightClear,
+  WiDayCloudy,
+  WiNightAltCloudy,
+  WiCloud,
+  WiCloudy,
+  WiDayRainMix,
+  WiNightAltRainMix,
+  WiDayRain,
+  WiNightAltRain,
+  WiDayLightning,
+  WiNightAltLightning,
+  WiDaySnowWind,
+  WiNightAltSnowWind,
+  WiDayFog,
+  WiNightFog,
+} from "weather-icons-react";
 
 export default function WeatherIcon(props) {
   const codeMap = {
-    "01d": {WiDaySunny},
-    "01n": {WiNightClear},
-    "02d": {WiDayCloudy},
-    "02n": {WiNightAltCloudy},
-    "03d": {WiCloud},
-    "03n": {WiCloud},
-    "04d": {WiCloudy},
-    "04n": {WiCloudy},
-    "09d": {WiDayRainMix},
-    "09n": {WiNightAltRainMix},
-    "10d": {WiDayRain},
-    "10n": {WiNightAltRain},
-    "11d": {WiDayLightning},
-    "11n": {WiNightAltLightning},
-    "13d": {WiDaySnowWind},
-    "13n": {WiNightAltSnowWind},
-    "50d": {WiDayFog},
-    "50n": {WiNightFog}
+    "01d": <WiDaySunny size={90} color="#000" />,
+    "01n": <WiNightClear size={90} color="#000" />,
+    "02d": <WiDayCloudy size={90} color="#000" />,
+    "02n": <WiNightAltCloudy size={90} color="#000" />,
+    "03d": <WiCloud size={90} color="#000" />,
+    "03n": <WiCloud size={90} color="#000" />,
+    "04d": <WiCloudy size={90} color="#000" />,
+    "04n": <WiCloudy size={90} color="#000" />,
+    "09d": <WiDayRainMix size={90} color="#000" />,
+    "09n": <WiNightAltRainMix size={90} color="#000" />,
+    "10d": <WiDayRain size={90} color="#000" />,
+    "10n": <WiNightAltRain size={90} color="#000" />,
+    "11d": <WiDayLightning size={90} color="#000" />,
+    "11n": <WiNightAltLightning size={90} color="#000" />,
+    "13d": <WiDaySnowWind size={90} color="#000" />,
+    "13n": <WiNightAltSnowWind size={90} color="#000" />,
+    "50d": <WiDayFog size={90} color="#000" />,
+    "50n": <WiNightFog size={90} color="#000" />,
   };
-  
+
   return (
-    <div>
-    <{codeMap[props.code]} size={24} color='#000' />
-    </div>
+  <div>{codeMap[props.code]}</div>
   );
 }
